a823c56f645b81fb47e1afff5a17b4aa8d4b6f45
PhalgunAmancha <118758080+PhalgunAmancha@users.noreply.github.com>
2024-06-18 00:08:02 +0530

    Time: 0 ms (100%), Space: 7.3 MB (49.24%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int sum=0;
+        for(int i=5;i<=n;i=i*5)
+        {
+            sum=sum+(n/i);
+        }
+        return sum;
+    }
+};
