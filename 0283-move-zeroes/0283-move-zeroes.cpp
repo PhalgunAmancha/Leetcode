f45e0a68fd9d691ded01f691c288e2aa3494051e
PhalgunAmancha <118758080+PhalgunAmancha@users.noreply.github.com>
2024-06-25 16:05:11 +0530

    Time: 22 ms (19.31%), Space: 21.7 MB (12.72%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[count]);
+                count++;
+            }
+        }
+    }
+};
