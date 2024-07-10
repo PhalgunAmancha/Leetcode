8ed3403d3f58fae00df3a559293f307b2670e6ed
PhalgunAmancha <118758080+PhalgunAmancha@users.noreply.github.com>
2024-07-10 12:03:09 +0530

    Time: 7 ms (32.78%), Space: 16.6 MB (6.52%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -12,18 +12,18 @@ public:
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            // Check if x is present at mid
+           
             if (nums[mid] == x)
                 return true;
 
-            // If x greater, ignore left half
+            
             if (nums[mid] < x)
                 low = mid + 1;
-            // If x is smaller, ignore right half
+            
             else
                 high = mid - 1;
         }
-        // If we reach here, then element was not present
+        
         return false;
     }
 
@@ -31,7 +31,7 @@ public:
         int n = nums.size();
         int j = -1;
 
-        // Find the pivot point where the rotation happens
+        
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] > nums[i + 1]) {
                 j = i;
@@ -39,19 +39,19 @@ public:
             }
         }
 
-        // If the array is not rotated
+        
         if (j == -1) {
             return binarySearch(nums, 0, n - 1, target);
         }
 
-        // Reverse the first part
+        
         reverse(nums, 0, j);
-        // Reverse the second part
+        
         reverse(nums, j + 1, n - 1);
-        // Reverse the entire array
+       
         reverse(nums, 0, n - 1);
 
-        // Perform binary search on the sorted array
+        
         return binarySearch(nums, 0, n - 1, target);
     }
 };
